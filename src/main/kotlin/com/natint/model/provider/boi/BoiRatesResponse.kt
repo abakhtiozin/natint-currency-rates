4823adf909aca2c977c7c5083312432a8f4ec29d
abakhtiozin <andrii.bakhtiozin@gmail.com>
2017-06-12 19:07:24 +0300

    handle null and empty values in response xml

--- src/main/kotlin/com/natint/model/provider/boi/BoiRatesResponse.kt
+++ src/main/kotlin/com/natint/model/provider/boi/BoiRatesResponse.kt
@@ -1,10 +1,10 @@
 package com.natint.model.provider.boi
 
-import com.natint.model.CurrencyCode
-import com.natint.model.CurrencyCode.valueOf
+import com.natint.model.CurrencyCode.*
 import com.natint.model.Rate
 import com.natint.model.Rates
 import com.natint.model.provider.boi.xml.mapping.Currencies
+import com.natint.model.provider.boi.xml.mapping.Currency
 import org.apache.commons.io.IOUtils
 import java.io.IOException
 import java.text.ParseException
@@ -37,22 +37,24 @@ internal class BoiRatesResponse(private val body: String) {
     @Throws(ParseException::class)
     private fun fillCurrencyRateList(currencies: com.natint.model.provider.boi.xml.mapping.Currencies): Rates {
         val xmlCurrencies = currencies.currencies
-        val ratesList = xmlCurrencies.map({
-            val code = it.currencycode.toUpperCase()
-            val rate = it.rate.toDouble()
-            Rate(valueOf(code), rate)
-        })
+        val ratesList = xmlCurrencies
+                .filterNotNull()
+                .filter { !it.currencycode.isNullOrBlank() && !it.rate.isNullOrBlank() }
                 .filter { byRates(it) }
+                .map {
+                    val code = it.currencycode.toUpperCase()
+                    val rate = it.rate.toDouble()
+                    Rate(valueOf(code), rate)
+                }
+
         val rateDate = getRateDate(currencies.last_update)
         return Rates(rateDate, ratesList)
     }
 
-    private fun byRates(it: Rate) = it.currencyCode in listOf(CurrencyCode.USD, CurrencyCode.EUR, CurrencyCode.GBP)
+    private fun byRates(it: Currency) = valueOf(it.currencycode.toUpperCase()) in listOf(USD, EUR, GBP)
 
     private fun getRateDate(last_update: String): LocalDate {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         return LocalDate.parse(last_update, formatter)
     }
 }
-
-
