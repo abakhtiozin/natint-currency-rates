c3600b0815105afa5693c30d63f637a07681b513
abakhtiozin <andrii.bakhtiozin@gmail.com>
2017-06-26 21:18:39 +0300

    move generating request url from method to field

--- src/main/kotlin/com/natint/model/provider/boi/BoiRatesRequest.kt
+++ src/main/kotlin/com/natint/model/provider/boi/BoiRatesRequest.kt
@@ -16,18 +16,23 @@ internal class BoiRatesRequest(
     private val logger = LoggerFactory.getLogger(this.javaClass)
     private var attempts = 3
 
+    private val urlWithParams = run {
+        val pattern = "yyyyMMdd"
+        val dateInString = date.format(DateTimeFormatter.ofPattern(pattern))
+        "$url$dateInString"
+    }
+
     internal fun send(): BoiRatesResponse {
-        val requestUrl = prepareUrl()
         val responseEntity: ResponseEntity<String>? = try {
-            restTemplate.getForEntity(requestUrl, String::class.java)
+            restTemplate.getForEntity(urlWithParams, String::class.java)
         } catch (e: ResourceAccessException) {
             logger.error("exception", e)
+            Thread.sleep(3000) //boi site tends to return error from time to time
             null
         }
         val body = responseEntity?.body?.toLowerCase() ?: ""
-        if (body.isEmpty() || body.hasErrors()) {
+        if (body.isEmpty() || body.hasNoRates()) {
             attempts--
-            Thread.sleep(3000) //boi site tends to return error from time to time
             while (attempts > 0) {
                 return send()
             }
@@ -35,13 +40,7 @@ internal class BoiRatesRequest(
         return BoiRatesResponse(date, body)
     }
 
-    private fun prepareUrl(): String {
-        val pattern = "yyyyMMdd"
-        val dateInString = date.format(DateTimeFormatter.ofPattern(pattern))
-        return "$url$dateInString"
-    }
-
-    private fun String.hasErrors(): Boolean {
+    private fun String.hasNoRates(): Boolean {
         return !this.contains("currencies", true)
     }
 }
